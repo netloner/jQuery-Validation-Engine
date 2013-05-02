a5dedf0098502bcaff723adae5e50a5336285c9c
Edward <netloner@gmail.com>
2013-05-02 15:54:08 +0800

    change attr to prop for radio button

--- js/jquery.validationEngine.js
+++ js/jquery.validationEngine.js
@@ -920,7 +920,7 @@
 				case "checkbox":
 					// new validation style to only check dependent field
 					if (condRequired) {
-						if (!field.attr('checked')) {
+						if (!field.prop('checked')) {
 							return options.allrules[rules[i]].alertTextCheckboxMultiple;
 						}
 						break;
